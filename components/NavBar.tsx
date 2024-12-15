82e309d7948cf2c0c95ada79bb23de6a361bc11c
Shen Sandaru <136662438+ShenSandaru@users.noreply.github.com>
2024-12-15 23:22:29 +0530

    make changes to tours and nav bar for responsive

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,6 +7,10 @@ import styles from '../styles/Navbar.module.css';
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className={styles.navbar}>
       <div className={`${styles.logo} ${isOpen ? styles.hidden : ''}`}>
@@ -20,11 +24,11 @@ const Navbar = () => {
       </div>
 
       <ul className={`${styles.navLinks} ${isOpen ? styles.active : ''}`}>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/destinations">Destinations</Link></li>
-        <li><Link href="/tours">Tours</Link></li>
-        <li><Link href="/about">About</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
+        <li><Link href="/" onClick={handleLinkClick}>Home</Link></li>
+        <li><Link href="/destinations" onClick={handleLinkClick}>Destinations</Link></li>
+        <li><Link href="/tours" onClick={handleLinkClick}>Tours</Link></li>
+        <li><Link href="/about" onClick={handleLinkClick}>About</Link></li>
+        <li><Link href="/contact" onClick={handleLinkClick}>Contact</Link></li>
       </ul>
     </nav>
   );
