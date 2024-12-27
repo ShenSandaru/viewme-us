2096a2959a5816090e2dbbab04afa7704f3a76b4
Shen Sandaru <136662438+ShenSandaru@users.noreply.github.com>
2024-12-27 18:22:20 +0530

    created the login button to the nav bar

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+
+export default function Head() {
+  return (
+    <>
+      <link
+        rel="icon"
+        href="https://i.ibb.co/vshrMk7/fav-viewme.png"
+        type="image/png"
+      />
+    </>
+  );
+}
